22b0ad5d31c31a26eab349cf784939cd95b087cc
IamMinhLong <longtmse140599@fpt.edu.vn>
2022-02-27 04:31:26 -0500

    init

---
+++ modules/tftp_enum.py
@@ -0,0 +1,7 @@
+import nmap
+nm=nmap.PortScanner()
+def Tftp_enum():
+    ip=input("Enter ip target:")
+    ip_addr=str(ip)
+    result=nm.scan(ip_addr,'69','-sU --script tftp-enum')
+    return result
