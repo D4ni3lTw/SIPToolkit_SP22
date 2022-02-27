92b3d24170e279282a3ec645dc76f2a55dda87aa
IamMinhLong <longtmse140599@fpt.edu.vn>
2022-02-27 05:50:30 -0500

    update

--- modules/tftp_enum.py
+++ modules/tftp_enum.py
@@ -1,8 +1,27 @@
 import nmap
 nm=nmap.PortScanner()
-def Tftp_enum():
+
+print("_____TFTP-enumeration_____")
+def Tftp_enum():#Scan target
     ip=input("Enter ip target:")
     ip_addr=str(ip)
     result=nm.scan(ip_addr,'69','-sU --script tftp-enum')
     return result
-print('hello')
+
+def getdata():# Get data from json result
+    data=Tftp_enum()
+    if data is None:
+        print("No Data!!!")
+    else:
+        method=data['nmap']['scaninfo']['udp']['method']
+        command_line=data['nmap']['command_line']
+        port = data['nmap']['scaninfo']['udp']['services']
+        state=data['scan']['192.168.50.145']['udp'][69]['state']
+        service=data['scan']['192.168.50.145']['udp'][69]['name']
+        file_enum=data['scan']['192.168.50.145']['udp'][69]['script']['tftp-enum']
+        print("Command: ",command_line)
+        print("\nPort is running at: ",port)
+        print("State is: ",state)
+        print("Service is running: ",service)
+        print("List file:",file_enum)
+getdata()
