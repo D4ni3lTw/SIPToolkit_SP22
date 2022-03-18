169409a0b382b2590a1324b2a8f9f2c9492eef20
hauhpp <phuchau248@live.com>
2022-03-19 01:51:29 +0700

    update

---
+++ modules/reporter/utility.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from jinja2 import Environment, FileSystemLoader
+import json
+
+
+def get_vul_data():
+    with open("reporting/vul_data.json", 'r', encoding='UTF-8') as file:
+        result = json.loads(file.read())
+        return [result['results'] if 'results' in result else [], result['total'] if 'total' in result else 0]
+
+
+def get_scanning_data():
+    with open("reporting/scan_data.json", 'r', encoding='UTF-8') as file:
+        result = json.loads(file.read())
+        return result
+
+
+def get_template():
+    env = Environment(loader=FileSystemLoader('reporting/templates'))
+    return env.get_template('sample.html')
+
+
+def tabling_data(index, columns, data):
+    df = pd.DataFrame(data, columns=columns, index=index)
+    return df.style
+
+
+def render_vulnerability_header(type, name):
+    result = '<div class="vulnerability {0}">{1}</div>'.format(type, name)
+    return result
+
+
+def render_information(header, body):
+    result = '<div><p class="title">{0}</p><hr class="solid">{1}</div>'.format(
+        header, body)
+    return result
+
+
+def render_body(title, content):
+    result = '<p>{0} <span class="content">{1}</span></p>'.format(
+        title, content)
+    return result
+
+
+def convert_array_data(array):
+    ref = ''
+    for i in array:
+        ref += render_body(i, "")
+    return ref
+
+
+def save_file(html, ip_addr):
+    with open('{0}.html'.format(ip_addr), 'w') as f:
+        f.write(html)
