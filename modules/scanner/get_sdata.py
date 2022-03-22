5a43eabf2de3193037669ed332f3fb5b47f5a181
D4ni3lTw <tom14756@gmail.com>
2022-03-23 03:10:27 +0700

    add cpe spliter

--- modules/scanner/get_sdata.py
+++ modules/scanner/get_sdata.py
@@ -14,6 +14,8 @@ def data_parse(ips, data):
             hostname = scan_data[str(singleip)]['hostnames'][0]['name']
             if 'mac' in scan_data[str(singleip)]['addresses']:
                 macaddr = scan_data[str(singleip)]['addresses']['mac']
+            else:
+                macaddr = ''
             if 'vendor' in scan_data[str(singleip)]:
                 vendor = scan_data[str(singleip)]['vendor']
 
@@ -33,6 +35,20 @@ def data_parse(ips, data):
                     if 'script' in tcp:
                         port_script = tcp[port]['script']
 
+            if 'udp' in scan_data[str(singleip)]:
+                udp = scan_data[str(singleip)]['udp']
+                u_portlist = list(udp.keys())
+                for u_port in u_portlist:
+                    u_port_num = u_port
+                    u_port_state = udp[port]['state']
+                    u_port_state_reason = udp[port]['reason']
+                    u_port_name = udp[port]['name']
+                    u_port_product = udp[port]['product']
+                    u_port_product_version = udp[port]['version']
+                    u_port_cpe = udp[port]['cpe']
+                    if 'script' in udp:
+                        u_port_script = udp[port]['script']
+
             if 'osmatch' in scan_data[str(singleip)]:
                 osmatch = scan_data[str(singleip)]['osmatch'][0]
                 osname = osmatch['name']
@@ -43,12 +59,31 @@ def data_parse(ips, data):
         print('Scanning Result!')
         print('---------------------------------------------------------------')
         print('Hostname:')
-        print(' -IPV4: ', singleip ,' - ', hostname)
-        # print(' -Mac address: ',macaddr)
-        print('OS Match:')
-        print(' -OS Name: ', osname)
-        print(' -Accuracy: ',osaccuracy)
-        print(' -CPE: ', oscpe)
+        if hostname != '':
+            print(' -IPV4: ', singleip ,' - ', hostname)
+        else:
+            print(' -IPV4: ', singleip)
+        print(' -Host status: ', host_state)
+        if macaddr != '':
+            print(' -Mac address: ',macaddr)
+        if host_state == 'up':
+            print('OS Match:')
+            print(' -OS Name: ', osname)
+            print(' -Accuracy: ',osaccuracy)
+            print(' -CPE: ', oscpe)
+
+#From port scanning CPE into vendor-product
+def cpe_spliter(ips, data):
+    ip = ip_spliter(ips)
+    scan_data = data['scan']
+    for singleip in ip:
+        if scan_data is not None:
+            if 'tcp' in scan_data[str(singleip)]:
+                tcp = scan_data[str(singleip)]['tcp']
+                portlist = list(tcp.keys())
+                for port in portlist:
+                    port_num = port
+                    port_cpe = tcp[port]['cpe']
 
 
 
