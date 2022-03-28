e6d3492b6e0ee16786333ee8bf3cc1fb1b5df2d0
D4ni3lTw <tom14756@gmail.com>
2022-03-28 16:30:29 +0700

    add continue after finish

--- modules/initial/welcome_screen.py
+++ modules/initial/welcome_screen.py
@@ -3,7 +3,7 @@ import pyfiglet
 
 def banner(title = 'SIP Toolkit', description = '', columns = 100):
     if (title == '') or (columns == '') or ( title == '' and description == '') :
-        ascii_banner = pyfiglet.figlet_format('SIP Toolkit: A Telephony Pentest Tool', font='larry3d', width = int(columns))
+        ascii_banner = pyfiglet.figlet_format('SIP Toolkit', font='larry3d', width = int(columns))
         print(ascii_banner + "\n\033[1m\033[91m ~ By GSP22IA06\n\033[0m")
     else:
         ascii_banner = pyfiglet.figlet_format(title, font='larry3d', width = int(columns))
