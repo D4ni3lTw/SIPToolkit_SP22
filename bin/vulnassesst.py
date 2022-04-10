01a1d96b774417c0c00dc95e432d0c0e59c467a3
nlquynhnhu <nlquynhnhu18700@gmail.com>
2022-04-10 21:13:26 +0700

    bruteforceupdate

--- bin/vulnassesst.py
+++ bin/vulnassesst.py
@@ -1,5 +1,17 @@
 from modules.vulnassess import cve_search
+from py_console import console
+import sys
 
 def vulnassesst(vendor,product):
-    api = cve_search.get_api(vendor, product)
-    cve_search.get_data(api)
+    if ( vendor is not None and product is not None ):
+        try:
+            api = cve_search.get_api(vendor, product)
+            cve_search.get_data(api)
+            return api
+        except Exception as e:
+            console.error("An Error Occurred!")
+            console.error(e)
+            sys.exit(1)
+        except:
+            console.error("Unexpected Error Occurred")
+            sys.exit(1)
