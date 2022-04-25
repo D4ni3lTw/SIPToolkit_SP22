1ef55b0fe5bbf157405d52f13e9e87b524588e5d
hauhpp <phuchau248@live.com>
2022-04-25 20:34:52 +0700

    fix update

--- bin/vulnassesst.py
+++ bin/vulnassesst.py
@@ -3,14 +3,13 @@ from modules.vulnassess import cve_search
 
 
 def vulnassesst(ip, scan_data):
-    vul_data = get_file_by_each_step('vul_data')
+    vul_data = get_file_by_each_step('vul_data', ip)
     if vul_data is None:
         vul_data = cve_search.get_vul_data(ip, scan_data)
     if vul_data is not None:
         cve_search.print_data(vul_data)
-        save_file_by_each_step(vul_data,'vul_data')
+        save_file_by_each_step(vul_data, 'vul_data', ip)
         return vul_data
-    else: 
+    else:
         print("Something wrong when getting vulnerabilities :((((")
         return None
-        
--- modules/ultilities/result_cached.py
+++ modules/ultilities/result_cached.py
@@ -3,17 +3,18 @@ import json
 import os
 
 
-def save_file_by_each_step(result, fileName):
+def save_file_by_each_step(result, fileName, ip):
     jsonString = json.dumps(result)
-    path = f"result/{fileName}.json"
-    if not os.path.exists('result/'):
-        os.makedirs("result/",0o777)
-    jsonFile = open(path, "w")
+    path = f'result/{ip}/'
+    if not os.path.exists(path):
+        os.makedirs(path, 0o777)
+    jsonFile = open(f'{path}{fileName}.json', 'w')
     jsonFile.write(jsonString)
     jsonFile.close()
 
-def get_file_by_each_step(fileName):
-    path = f"result/{fileName}.json"
+
+def get_file_by_each_step(fileName, ip):
+    path = f'result/{ip}/{fileName}.json'
     try:
         with open(path, 'r', encoding='UTF-8') as file:
             result = json.loads(file.read())
