fe78f081bd8849926e4f71d5e078cb2f364002e5
D4ni3lTw <tom14756@gmail.com>
2022-04-26 12:39:54 +0700

    remove output data

--- bin/vulnassesst.py
+++ bin/vulnassesst.py
@@ -9,7 +9,6 @@ def vulnassesst(ip, scan_data, port_cpe=None):
     vul_data = get_file_by_each_step('vul_data', ip)
     if vul_data is None:
         vul_data = cve_search.get_vul_data(ip, scan_data, port_cpe)
-        print(vul_data)
     if vul_data is not None:
         cve_search.print_data(vul_data)
         save_file_by_each_step(vul_data, 'vul_data', ip)
